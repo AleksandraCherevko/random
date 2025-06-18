8cf6d2e1553404c222cc997bc38c61942478eba7
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-06-18 15:38:56 +0200

    update quotes

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 const quotes = [
-  "Simplicity is the ultimate sophistication. — Leonardo da Vinci",
-  "The less you have, the less you have to worry about.",
-  "Minimalism is not a lack of something. It’s simply the perfect amount.",
-  "Perfection is achieved not when there is nothing more to add, but when there is nothing left to take away.",
-  "Do less, but better.",
-  "Clarity comes from simplicity.",
+  {
+    text: "Simplicity is the ultimate sophistication.",
+    author: "Leonardo da Vinci",
+  },
+  {
+    text: "The less you have, the less you have to worry about.",
+    author: "Buddhist Proverb",
+  },
+  {
+    text: "Minimalism is not a lack of something. It’s simply the perfect amount.",
+    author: "Nicholas Burroughs",
+  },
+  {
+    text: "Do less, but better.",
+    author: "Dieter Rams",
+  },
+  {
+    text: "Perfection is achieved not when there is nothing more to add, but when there is nothing left to take away.",
+    author: "Antoine de Saint-Exupéry",
+  },
+  {
+    text: "Clarity comes from simplicity.",
+    author: "Greg McKeown",
+  },
+  {
+    text: "Simplicity boils down to two steps: Identify the essential. Eliminate the rest.",
+    author: "Leo Babauta",
+  },
+  {
+    text: "The ability to simplify means to eliminate the unnecessary so that the necessary may speak.",
+    author: "Hans Hofmann",
+  },
+  {
+    text: "Simplicity is the keynote of all true elegance.",
+    author: "Coco Chanel",
+  },
+  {
+    text: "Less is more.",
+    author: "Ludwig Mies van der Rohe",
+  },
 ];
 
 const quoteElement = document.querySelector("#quote");
@@ -13,6 +47,6 @@ const generateBtn = document.querySelector(".generate-btn");
 function showRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  quoteElement.textContent = `"${randomQuote.text}" — ${randomQuote.author}`;
 }
 generateBtn.addEventListener("click", showRandomQuote);
